4ca9e8e88adcd9373ff2e8784ce78b77e58c2a8a
Walter.Sama11 <Walter.Sama11@bb8660e3-ae87-c290-0b71-771867bc3479>
2014-12-17 08:29:42 +0000

    aggiunte in automatico le pagine controller

---
+++ MeteoCalProject/src/main/java/entity/BadconditionsFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Walter
+ */
+@Stateless
+public class BadconditionsFacade extends AbstractFacade<Badconditions> {
+    @PersistenceContext(unitName = "com.mycompany_MeteoCalProject_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public BadconditionsFacade() {
+        super(Badconditions.class);
+    }
+    
+}
