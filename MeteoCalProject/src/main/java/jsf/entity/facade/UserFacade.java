45736356801a842c31079838a80172507dd4a85f
claudio.ubugu <claudio.ubugu@bb8660e3-ae87-c290-0b71-771867bc3479>
2014-12-24 13:49:50 +0000

    rebuild project

---
+++ MeteoCalProject/src/main/java/jsf/entity/facade/UserFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf.entity.facade;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import jsf.entity.User;
+
+/**
+ *
+ * @author claudio
+ */
+@Stateless
+public class UserFacade extends AbstractFacade<User> {
+    @PersistenceContext(unitName = "com.mycompany_MeteoCalProject_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public UserFacade() {
+        super(User.class);
+    }
+    
+}
