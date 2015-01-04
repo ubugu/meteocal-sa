ae119b9bfb92fd4d51df25188efe10447dfcbaf8
claudio.ubugu <claudio.ubugu@bb8660e3-ae87-c290-0b71-771867bc3479>
2015-01-04 13:16:03 +0000

    finished shared users managing

--- MeteoCalProject/src/main/java/jsf/entity/cotroller/SettingsController.java
+++ MeteoCalProject/src/main/java/jsf/entity/cotroller/SettingsController.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package jsf.entity.cotroller;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,27 +22,27 @@ import org.primefaces.context.RequestContext;
  *
  * @author claudio
  */
-@ManagedBean( name= "settingsController" , eager = true)
+@ManagedBean(name = "settingsController", eager = true)
 @ViewScoped
 public class SettingsController {
-    
     private User user = new User();
     private Boolean setNewPassword = false;
 
     private Boolean setNewEmail = false;
-    
+
     private Boolean setSharedPrivacy = false;
-    
+
     private String privacy;
-    
+
     private String oldPassword = "";
-    
+
     private String sharedUsersString = "";
 
- 
+    private List<User> oldSharedUsers = new ArrayList<>();
+
     @EJB
     UserFacade userFacade = new UserFacade();
-    
+
     @EJB
     CalendarFacade calendarFacade = new CalendarFacade();
 
@@ -51,11 +52,25 @@ public class SettingsController {
             this.setSharedPrivacy = true;
         }
         List<User> allUser = this.userFacade.findAll();
-        
+        Calendar calendar = this.calendarFacade.searchByUser(this.userFacade.getLoggedUser());
+        List<Calendar> sharedCalendar;
+        for (User u : allUser) {
+            sharedCalendar = u.getCalendarList();
+            if (sharedCalendar == null) {
+                continue;
+            }
+
+            if (sharedCalendar.contains(calendar)) {
+                this.sharedUsersString += u.getUsername() + "; ";
+                this.oldSharedUsers.add(u);
+            }
+        }
+
     }
+
     public String getPrivacy() {
         if (privacy == null) {
-           privacy = calendarFacade.searchByUser(userFacade.getLoggedUser()).getPrivacy();
+            privacy = calendarFacade.searchByUser(userFacade.getLoggedUser()).getPrivacy();
         }
         return privacy;
     }
@@ -64,7 +79,7 @@ public class SettingsController {
         this.privacy = privacy;
         this.setSharedPrivacy = privacy.equals("SHARED");
     }
- 
+
     public User getUser() {
         return user;
     }
@@ -72,7 +87,7 @@ public class SettingsController {
     public void setUser(User user) {
         this.user = user;
     }
-    
+
     public Boolean getSetNewPassword() {
         return setNewPassword;
     }
@@ -120,7 +135,7 @@ public class SettingsController {
             return false;
         }
     }
-    
+
     private boolean checkEmail() {
         List<User> allUser = userFacade.findAll();
 
@@ -134,8 +149,8 @@ public class SettingsController {
         return false;
     }
 
-    
     public String updateSettings() {
+        
         if (!checkPassowrd()) {
             RequestContext requestContext = RequestContext.getCurrentInstance();
             requestContext.execute("PF('wrongPassword').show();");
@@ -144,23 +159,46 @@ public class SettingsController {
             Calendar calendar = this.calendarFacade.searchByUser(this.userFacade.getLoggedUser());
             calendar.setPrivacy(this.privacy);
             User addUser = null;
-            
+
             if (this.privacy.equals("SHARED")) {
                 List<String> sharedUsernames = getUsername(this.sharedUsersString);
-                List<User> sharedUsers = new ArrayList<>();
-                //Check if a wrong username is inserted
-                for (String s : sharedUsernames) {
-                    addUser = this.userFacade.searchForUser(s);
-                    if (addUser == null) {
-                        RequestContext requestContext = RequestContext.getCurrentInstance();
-                        requestContext.execute("PF('wrongUsername').show();");
-                        return "";
+                List<User> newSharedUsers = new ArrayList<>();
+        
+                if (!sharedUsernames.isEmpty()) {
+                    //Check if a wrong username is inserted
+                    for (String s : sharedUsernames) {
+                        addUser = this.userFacade.searchForUser(s);
+                        if (addUser == null) {
+                            RequestContext requestContext = RequestContext.getCurrentInstance();
+                            requestContext.execute("PF('wrongUsername').show();");
+                            return "";
+                        }
+                        newSharedUsers.add(addUser);
                     }
-                    sharedUsers.add(addUser);
+                }
+               
+
+                //Construct delete list and update newSharedUser so to not recreate sharedUsers if already presents in the database
+                List<User> deleteSharedUsers = new ArrayList<>();
+                for (User u : oldSharedUsers) {
+                    if (newSharedUsers.contains(u)) {
+                        newSharedUsers.remove(u);
+                    } else {
+                        deleteSharedUsers.add(u);
+                    }
                 }
                 
-                calendar.setUserList(sharedUsers);
-                for (User u : sharedUsers) {
+                //Delete new Shared Users
+                for (User u : deleteSharedUsers) {
+                    List<Calendar> deleteList = u.getCalendarList();
+                    deleteList.remove(calendar);
+                    u.setCalendarList(deleteList);
+                    this.userFacade.edit(u);
+                }
+                
+                
+                //Add new shared Users
+               for (User u : newSharedUsers) {
                     List<Calendar> calendarList = u.getCalendarList();
                     if (calendarList == null) {
                         calendarList = new ArrayList<>();
@@ -170,36 +208,47 @@ public class SettingsController {
                     this.userFacade.edit(u);
                 }
             }
-            
+
             calendarFacade.edit(calendar);
 
             User loggedUser = this.userFacade.getLoggedUser();
-            
+
             if ((!this.setNewEmail) && (!this.setNewPassword)) {
                 return "/settings?faces-redirect=true";
             }
-            
+
             if (!this.setNewEmail) {
                 this.user.setEmail(loggedUser.getEmail());
-            } else { 
+            } else {
                 if (checkEmail()) {
                     return "";
-                }   
+                }
             }
-            
+
             if (!this.setNewPassword) {
                 this.user.setAlreadyEncryptedPassword(loggedUser.getPassword());
             }
-            
+
             this.user.setUsername(loggedUser.getUsername());
             this.user.setGroupName("USER");
             this.userFacade.edit(user);
-            
+
             return "/settings?faces-redirect=true";
         }
     }
 
+    /**
+     * Method that gets substring divided by ; from a String.
+     * @param sharedUsers String of the input text; all users must be divided by ";"
+     * @return a List that contains the username of the shared users. if no username is entere an empy list is returne;
+     */
     private List<String> getUsername(String sharedUsers) {
+        sharedUsers = sharedUsers.trim();
+        
+        if (sharedUsers.length() == 0) {
+            return new ArrayList<>();
+        }
+
         String[] sharedUsername = sharedUsers.split(";");
         for (int i = 0; i < sharedUsername.length; i++) {
             sharedUsername[i] = sharedUsername[i].replaceAll(" ", "");
@@ -207,5 +256,4 @@ public class SettingsController {
         return Arrays.asList(sharedUsername);
     }
 
-    
 }
