182afdd2010970216cc03bf1fbd55e44f03371f3
Walter.Sama11 <Walter.Sama11@bb8660e3-ae87-c290-0b71-771867bc3479>
2015-01-05 19:57:29 +0000

    aggiunto tentativo di time schedule

---
+++ MeteoCalProject/src/main/java/managed/MyTimerTask.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managed;
+
+import java.util.TimerTask;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Walter
+ */
+@Stateless
+public class MyTimerTask extends TimerTask{
+       
+    @Override
+    public void run(){
+                
+        System.out.println("ok");
+        
+    } 
+    
+}
