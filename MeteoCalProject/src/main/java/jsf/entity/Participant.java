6b0319d1bddbde9bec193626506ae9ec8ea54742
Walter.Sama11 <Walter.Sama11@bb8660e3-ae87-c290-0b71-771867bc3479>
2015-01-19 21:35:02 +0000

    risolto bug

--- MeteoCalProject/src/main/java/jsf/entity/Participant.java
+++ MeteoCalProject/src/main/java/jsf/entity/Participant.java
@@ -28,6 +28,7 @@ import javax.xml.bind.annotation.XmlRootElement;
     @NamedQuery(name = "Participant.findAll", query = "SELECT p FROM Participant p"),
     @NamedQuery(name = "Participant.findByUser", query = "SELECT p FROM Participant p WHERE p.participantPK.user = :user"),
     @NamedQuery(name = "Participant.findAcceptedEventByUser", query = "SELECT p.event1 FROM Participant p WHERE p.participant = 'YES' AND p.participantPK.user = :user"),
+    @NamedQuery(name = "Participant.findByUserEvent", query = "SELECT p FROM Participant p WHERE p.participantPK.user = :user AND p.participantPK.event = :event"),
     @NamedQuery(name = "Participant.findByEvent", query = "SELECT p FROM Participant p WHERE p.participantPK.event = :event"),
     @NamedQuery(name = "Participant.findByOrganiser", query = "SELECT p FROM Participant p WHERE p.organiser = :organiser"),
     @NamedQuery(name = "Participant.findByParticipant", query = "SELECT p FROM Participant p WHERE p.participant = :participant")})
